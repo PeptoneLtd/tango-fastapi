2cdecdfa884fb560312f6adadc27ac3ab207f1df
Fabio Airoldi <fabio@peptone.io>
2022-09-21 10:13:02 +0200

    use std port

--- tango/api.py
+++ tango/api.py
@@ -51,4 +51,4 @@ async def v1_tango_aggregation(req: TangoRequest) -> List[float]:
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8006)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
